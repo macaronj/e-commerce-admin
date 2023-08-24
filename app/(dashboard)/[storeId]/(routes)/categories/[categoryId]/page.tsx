5aa0aaf41f222a0f0159e11947062cb7eef6df16
macaronj <macaronj@protonmail.com>
2023-08-24 13:29:14 +0200

    Fix: Remove console.log

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -18,8 +18,6 @@ const CategoryPage = async ({
     },
   });
 
-  console.log("billboards", billboards);
-
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
