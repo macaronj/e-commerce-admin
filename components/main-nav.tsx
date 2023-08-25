c46d156f5c9b188a15a18d97db6e96bfa15ac012
macaronj <macaronj@protonmail.com>
2023-08-25 23:13:39 +0200

    Fix: Navbar scrolling

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -54,7 +54,12 @@ export function MainNav({
     },
   ];
   return (
-    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+    <nav
+      className={cn(
+        "flex items-center space-x-4 lg:space-x-6 overflow-x-auto",
+        className
+      )}
+    >
       {routes.map((route) => (
         <Link
           key={route.href}
